08162cfe6ca7d87fed5def7ede84ec5328eb89dc
mosidrum <mosiokanga@gmail.com>
2024-02-08 21:19:05 +0100

    Add database connection

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./connectDB");
+const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+// connects the database
+connectDB();
+
+// this allows cross origin resource sharing between client and server, which is most times on different port
+app.use(cors());
+// this is needed to pass data as json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// default route
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
+  res.json("Welcome to our CMS server!");
+});
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000")
-})
+// 404 route (for routes that don't exist)
+app.get("*", (req, res) => {
+  res.sendStatus("404");
+});
+
+// comand-line check
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
